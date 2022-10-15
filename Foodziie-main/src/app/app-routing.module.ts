bc15f671ecfea377eb681db31b7490e2580c0d69
Anik Duley <Anik.Duley@mjunction.in>
2022-10-15 23:57:03 +0530

    first commit

---
+++ Foodziie-main/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
+import { FoodPageComponent } from './components/pages/food-page/food-page.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { LoginCComponent } from './login-c/login-c.component';
+import { LoginMerchantComponent } from './login-merchant/login-merchant.component';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'search/:searchTerm', component: HomeComponent },
+  { path: 'tag/:tag', component: HomeComponent },
+  {path:'food/:id', component:FoodPageComponent},
+  {path:'cart-page', component: CartPageComponent},
+  {path:'loginCustomer',component:LoginCComponent},
+  {path:'loginMerchant',component:LoginMerchantComponent},
+  {path:'register',component:RegisterComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Foodziie-main/src/app/login-merchant/login-merchant.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-merchant',
+  templateUrl: './login-merchant.component.html',
+  styleUrls: ['./login-merchant.component.css']
+})
+export class LoginMerchantComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
